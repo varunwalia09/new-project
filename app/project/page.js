3a2a559f43837d46f02dc9eb2a356ac6cf9d8b19
varunwalia09 <walia3616@gmail.com>
2025-06-06 10:17:19 +0530

    again i create the next.js new project then again i create a new repo and push the code

---
+++ app/project/page.js
@@ -0,0 +1,12 @@
+
+'use client'
+import Project from '@/components/Project/Project'
+export default function Page() {
+  return (
+    <>
+    <Project/>
+      {/* <Footer/> */}
+    </>
+  );
+}
+
